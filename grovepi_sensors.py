1f2d895fcb3cdd99ae24222998c376984dd4bb44
Luis-the-Third <luisagon@usc.edu>
2025-03-06 17:04:22 -0800

    Update grovepi_sensors.py

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -12,29 +12,34 @@ grovepi.pinMode(potentiometer,"INPUT")
 
 # clear lcd screen  before starting main loop
 setText("")
+string = ""
 
 while True:
   try:
     # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
+    
+
     distance = grovepi.ultrasonicRead(ultrasonic_ranger)
-    setCursor(0, 0)
-    setText("D: " + str(distance) + "cm")
 
 
     # TODO: read threshold from potentiometer
     threshold = grovepi.analogRead(potentiometer)
-    setCursor(0, 1)
-    setText("T: " + str(threshold) + "cm")
 
     # TODO: format LCD text according to threshold
+    
     if(distance > threshold):
       setRGB(255, 0, 0)
-      setCursor(10, 0)
-      setText("OBJ PRES")
+      string = f"D: {distance}cm  OBJ PRES\nT: {threshold}cm"
+
     else:
       setRGB(0, 255, 0)
-      setCursor(10, 0)
-      setText("No obj pres")
+      string = f"D: {distance}cm  NO OBJ\nT: {threshold}cm"
+
+
+    
+    setText(string)
+
+
 
 
   
